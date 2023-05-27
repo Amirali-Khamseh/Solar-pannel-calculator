55154b452f4b391bb0851644e77553752ff053cf
Amir <72108341+Amirali-Khamseh@users.noreply.github.com>
2023-05-27 11:35:20 +0200

    MVC pattern implemented + connection to mongodb completed

---
+++ Back/controllers/authController.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+signup = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            email: email,
+            password: hashedPassword,
+        });
+
+        await user.save();
+
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        console.error('Error signing up', error);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+};
+
+signin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        res.status(200).json({ message: 'Sign in successful' });
+    } catch (error) {
+        console.error('Error signing in', error);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+};
+
+
+module.exports = { signin, signup };
--- Back/server.js
+++ Back/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
-
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 const port = process.env.PORT || 3000;
+const { connectionDB } = require('../Back/db/config')
 
+//connectionDB 
+connectionDB();
 
 //middleware set up 
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +17,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello')
 })
+app.use('/auth', authRoutes);
 
 app.listen(port, () => {
     console.log('Server up and running on port ' + port
